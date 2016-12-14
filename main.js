39b39471b0e94c0f048076caede6e0c0d81ad321
Ted Newman <zippydee111@gmail.com>
2016-12-14 13:39:48 -0800

    Basic parsing
    Added basic parsing. Turns all "LUL"s in chat bold and colors them red.

--- main.js
+++ main.js
@@ -29,6 +29,46 @@ var chatObserver = new MutationObserver(function (mutations) {
 });
 
 
-var parseMsgHTML = function (msgHTML) {
-	console.log(msgHTML[0])
+var parseMsgHTML = function (msg) {
+	var msgElement = msg.find(".message")
+	
+	var contents = msgElement[0].childNodes
+	var s = ""
+	for (var i = 0; i < contents.length; i++){
+		var e = contents[i];
+		if (e instanceof Text){
+			//Check text element for unparsed emotes
+			if (e.data){
+				if(e.data.indexOf("LUL") > -1){
+					s += "<b style='color:red;'>LUL</b>"
+				}else{
+					s += e.data
+				}
+			}
+		}
+		else if (e){
+			s += e.outerHTML
+		}
+	}
+	msgElement.html(s)
 };
+
+/*
+
+Message Format:
+
+<li id="ember4881" class="ember-view message-line chat-line"><div class="indicator"></div>
+<!---->  <span class="timestamp float-left">12:30</span>
+<!----><!---->  <span class="badges float-left">
+<!---->  </span>
+  <span class="from" style="color:#NAMECOLOR" data-ember-action="4882">TWITCH_USERNAME</span>
+  <span class="colon">:</span>
+      <span class="message" style="">
+            SOME TEXT
+            <span class="balloon-wrapper">
+              <img class="emoticon" src="//static-cdn.jtvnw.net/emoticons/v1/245/1.0" srcset="//static-cdn.jtvnw.net/emoticons/v1/245/2.0 2x" alt="ResidentSleeper">
+              <div class="balloon balloon--tooltip balloon--up balloon--center mg-t-1">ResidentSleeper</div>
+            </span>
+      </span>
+<!----><!----></li>
+*/ 
