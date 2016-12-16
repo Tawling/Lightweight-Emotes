052d60a57ce67c5cb0fbf0bfe8f9a3526b9e3a65
Tawling <zippydee111@gmail.com>
2016-12-15 20:49:02 -0800

    Cross-page support, separate emote storage
    Global and channel emotes stored separately
    script persists when changing pages

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var config = {attributes: false, childList: true, characterData: false};
 var emotes = [];
+var channelEmotes = [];
+var globalEmotes = [];
 var foundChat = false;
 var htmlBody = $("body")[0];
 var channel = ""
@@ -15,23 +17,23 @@ var chatLoadedObserver = new MutationObserver(function (mutations, observer) {
         var chatSelector = $(".chat-lines");
         if (chatSelector.length > 0) {
 			if (!foundChat){
+				console.log("Found Chat");
 				var target = chatSelector[0];
 				foundChat = true;
-				fetchEmotes(function(){
-					chatObserver.observe(target, config);
-					console.log("EMOTES: ",emotes);
-				});
+				getChannel(target)
 				//observer.disconnect();
 			}
         }else {
+			if (foundChat) console.log("Lost Chat");
 			foundChat = false;
 			emotes = []
+			channelEmotes = []
 			chatObserver.disconnect();
 		}
     })
 });
 
-var getChannel = function(){
+var getChannel = function(target){
 	console.log(window.location.pathname)
 	var loc = window.location.pathname.split("/")[1]
 	console.log(loc)
@@ -39,8 +41,11 @@ var getChannel = function(){
 		channel = channelDisplay = loc.toLowerCase();
 		$.getJSON("https://api.twitch.tv/kraken/users/"+channel,function(data){
 			channelDisplay = data["display_name"]
-		}).fail(function(){console.log("error getting stuff")}).always(function(){
-			chatLoadedObserver.observe(htmlBody, config);
+		}).fail(function(){console.log("Error retrieving display name")}).always(function(){
+			fetchEmotes(function(){
+				chatObserver.observe(target, config);
+				console.log("EMOTES: ",emotes);
+			});
 		});
 	}else{
 		console.log("Couldn't identify channel...Where are you?")
@@ -49,7 +54,7 @@ var getChannel = function(){
 	
 }
 
-getChannel()
+chatLoadedObserver.observe(htmlBody, config);
 
 // Attach listener that acts when a new chat message appears.
 var chatObserver = new MutationObserver(function (mutations) {
@@ -68,7 +73,7 @@ var chatObserver = new MutationObserver(function (mutations) {
 
 var parseMsgHTML = function (msg) {
 	var msgElement = msg.find(".message")
-	
+	if (!msgElement[0]) return;
 	var contents = msgElement[0].childNodes
 	var s = ""
 	for (var i = 0; i < contents.length; i++){
@@ -81,27 +86,32 @@ var parseMsgHTML = function (msg) {
 			s += e.outerHTML
 		}
 	}
-	msgElement.html(s)
+	msgElement.html(s);
 };
 
 var processText = function(text,n){
-	n = n || 0
-	if (n > 20) return text;
-	if (text){
+	n = n || 0;
+	if (n > 1000) return text; //No infinite recursion pls.
+	if (text && text.trim()){
 		var s = ""
 		for (var i = 0; i < emotes.length; i++){
 			var emote = emotes[i];
-			var re = RegExp("(?:^|\\s|&nbsp;|&NBSP;)(" + RegExp.escape(emote.name) + ")(?=$|\\s+)","g");
-			var match = re.exec(text);
-			if(match){
-				s += processText(text.substring(0,re.lastIndex - emote.name.length), n+1) +
-					'<span class="balloon-wrapper">' +
-					'<img class="emoticon" src="' + emote.url + '" alt="'+emote.name+'">' +
-						'<div class="balloon balloon--tooltip balloon--up balloon--right mg-t-1">' + '<center>Emote: ' + emote.name + "<br />" + emote.setName + '</center></div>'+
-					'</span>';
-					//"<b style='color:red;'>" +emote.name + "</b>";
-				text = text.substring(re.lastIndex);
-				i--;
+			if (text.length >= emote.name.length){
+				var re = emote.re;
+				re.lastIndex = 0;
+				var match = re.exec(text);
+				var lastIndex = re.lastIndex
+				if(match){
+					s += processText(text.substring(0,lastIndex - emote.name.length), n+1) +
+						'<span class="balloon-wrapper">' +
+						'<img class="emoticon" src="' + emote.url + '" alt="'+ emote.name +'">' +
+							'<div class="balloon balloon--tooltip balloon--up balloon--right mg-t-1">' +
+								'<center>Emote: ' + emote.name + "<br />" + emote.setName + '</center>' +
+							'</div>'+
+						'</span>';
+					text = text.substring(lastIndex);
+					i--;
+				}
 			}
 		}
 		return s + text;
@@ -109,32 +119,36 @@ var processText = function(text,n){
 	return text;
 }
 
-/*
- <span class="balloon-wrapper">
-              <img class="emoticon" src="//static-cdn.jtvnw.net/emoticons/v1/245/1.0" srcset="//static-cdn.jtvnw.net/emoticons/v1/245/2.0 2x" alt="ResidentSleeper">
-              <div class="balloon balloon--tooltip balloon--up balloon--center mg-t-1">ResidentSleeper</div>
-            </span>
-*/
-
 
 var fetchEmotes = function(callback){
 	console.log(emotes)
-	emotes = []
-	getBTTVEmotes(null,function(){
-		getBTTVEmotes(channel,function(){
-			getFFZEmotes(null,function(){
-				getFFZEmotes(channel,function(){
-					if(callback) callback();
+	if (globalEmotes.length){
+		getFFZEmotes(null,function(){
+			getFFZEmotes(channel,function(){
+				emotes = globalEmotes.concat(channelEmotes)
+				if(callback) callback();
+			});
+		});
+	}else{
+		getBTTVEmotes(null,function(){
+			getBTTVEmotes(channel,function(){
+				getFFZEmotes(null,function(){
+					getFFZEmotes(channel,function(){
+						emotes = globalEmotes.concat(channelEmotes)
+						if(callback) callback();
+					});
 				});
 			});
 		});
-	});
+	}
 }
 
 var getBTTVEmotes = function(chan,callback){
 	callback = callback || function(){};
 	var url = "//api.betterttv.net/2/emotes/"
 	if (chan) url = "//api.betterttv.net/2/channels/" + chan.toLowerCase();
+	var emotes = globalEmotes;
+	if (chan) emotes = channelEmotes;
 	console.log(url)
 	$.getJSON(url, function(data){
 		if (data.error) return null;
@@ -150,7 +164,8 @@ var getBTTVEmotes = function(chan,callback){
 				image: emote.image,
 				url: emote.image.src,
 				id: emote.id,
-				ext: "BTTV"
+				ext: "BTTV",
+				re: RegExp("(?:^|\\s|&nbsp;|&NBSP;)(" + RegExp.escape(emote.code) + ")(?=$|\\s+)","g")
 			});
 		}
 	}).fail(function(){console.log("ERROR")}).always(callback);
@@ -158,50 +173,31 @@ var getBTTVEmotes = function(chan,callback){
 
 var getFFZEmotes = function(chan,callback){
 	callback = callback || function(){};
-	if (chan) {
-		var url = "//api.frankerfacez.com/v1/room/" + chan.toLowerCase()
-		$.getJSON(url, function(data){
-			for (var i in data["sets"]){
-				var set = data["sets"][i];
-				for (var j in set["emoticons"]){
-					var emote = set["emoticons"][j];
-					emote.image = new Image();
-					emote.image.src = emote.urls[1]
-					
-					emotes.push({
-						name: emote.name,
-						owner: emote.owner.display_name,
-						image: emote.image,
-						url: emote.image.src,
-						id: emote.id,
-						ext: "FFZ"
-					})
-				}
+	var url = "//api.frankerfacez.com/v1/set/global"
+	if (chan) url = "//api.frankerfacez.com/v1/room/" + chan.toLowerCase()
+	var emotes = globalEmotes;
+	if (chan) emotes = channelEmotes;
+	$.getJSON(url, function(data){
+		for (var i in data["sets"]){
+			var set = data["sets"][i];
+			for (var j in set["emoticons"]){
+				var emote = set["emoticons"][j];
+				emote.image = new Image();
+				emote.image.src = emote.urls[1]
+				
+				emotes.push({
+					name: emote.name,
+					owner: emote.owner.display_name,
+					setName: (set.title == "Global Emoticons" ? "FFZ Global Emoticons" : set.title),
+					image: emote.image,
+					url: emote.image.src,
+					id: emote.id,
+					ext: "FFZ",
+					re: RegExp("(?:^|\\s|&nbsp;|&NBSP;)(" + RegExp.escape(emote.name) + ")(?=$|\\s+)","g")
+				})
 			}
-		}).always(callback);
-	} else {
-		var url = "//api.frankerfacez.com/v1/set/global"
-		$.getJSON(url, function(data){
-			for (var i in data["sets"]){
-				var set = data["sets"][i];
-				for (var j in set["emoticons"]){
-					var emote = set["emoticons"][j];
-					emote.image = new Image();
-					emote.image.src = emote.urls[1]
-					
-					emotes.push({
-						name: emote.name,
-						owner: emote.owner.display_name,
-						setName: set.title,
-						image: emote.image,
-						url: emote.image.src,
-						id: emote.id,
-						ext: "FFZ"
-					})
-				}
-			}
-		}).always(callback);
-	}
+		}
+	}).always(callback);
 }
 
 /*
